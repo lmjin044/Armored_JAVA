de9945c282812512b44b4c809a661daccda93541
lmjin044 <b_pen@naver.com>
2024-07-02 17:30:54 +0900

    1

--- java0702_Start/src/java0702_2/Study_0702_4.java
+++ java0702_Start/src/java0702_2/Study_0702_4.java
@@ -5,16 +5,48 @@ public class Study_0702_4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int num = 300;
+		//메서드 중괄호, if문 등에서 만들어지는 변수는 지역변수
+		//그러므로 이 변수가 만들어진 영역에서만 사용가능.
+
+		output(num);
+		//서순이 중요함. 
+		//static 요소가 먼저 만들어지고 그 다음 void 요소가 만들어짐
+		//이 경우 static 하위에 output을 만든다고 해도 정상 작동되지 않음.
+		
 		
 	}
 
-	
+	static void output(int a) {	
+		//num이 가진 데이터 타입과 이 중괄호 내에서 만들어진 변수명
+		//a가 output을 통해 num의 값을 저장받음.
+		System.out.println(a);
+		System.out.println("나는 메서드 입니다.");
+		//인스턴스 메서드.
+		//output을 실행시키려면 이쪽도 static으로 해야 함.
+		//클래스 메서드에서는 오직 클래스 변수-메서드만 실행 :
+		//메모리에 로드(적재)되는 때가 인스턴스보다 먼저 이루어지기 때문
+		// >> 인스턴스변수, 인스턴스 메서드는 작동하지 않음.
+		//상단의 num을 출력하고 싶어도 일단 이 중괄호 안에서 생성된 변수가 아님
+		//output을 매개변수로 삼아서 가지고 오는 방법
+	}
 	
 }
-//main 메서드 : 별도의 호출 없이도 가장 먼저 실행되는 부분
-//실행하려면 JVM(Java Virtual Machine)이 필요함
-
 /*
+class a{
+	int num =10;
+	static void out() {	//static이 붙었으므로 클래스 메서드 : 객체를 만들지 않아도 실행 가능.
+		System.out.println(num);
+	}
+}
+//이렇게 하겠다고 하는 일종의 선언. new 연산자를 통해 공간을 만들지 않아 num 안에는 아무것도 없다.
+
+
+//main 메서드 : 별도의 호출 없이도 가장 먼저 실행되는 부분
+//실행하려면 JVM(Java Virtual Machine)이 필요함 : 얘가 먼저 받고 윈도우에 넘겨줌.
+//
+
+
  메서드? 클래스의 기능에 해당하는 요소
  구성 요건 : 반환될타입 메서드이름(매개변수){메서드 내용}
   1)반환될 타입 - int, short, flaot, double, char, boolean, void, String 등
