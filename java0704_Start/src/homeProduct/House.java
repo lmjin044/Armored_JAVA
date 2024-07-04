16faffb795bb2ce5aafd65559b3b9f11e4cc306a
lmjin044 <b_pen@naver.com>
2024-07-05 08:46:00 +0900

    1

--- java0704_Start/src/homeProduct/House.java
+++ java0704_Start/src/homeProduct/House.java
@@ -1,5 +1,6 @@
 package homeProduct;
-
+//tv와 에어컨에 만들었던 것을 아예 House로 빼버림.
+//그럼 tv랑 에어컨과 컴퓨터도 상속을 받아서 여기서 만든 power 메서드를 사용 가능
 public class House {
 	boolean onOff;
 	String brand;	//제품 브랜드명
@@ -8,10 +9,15 @@ public class House {
 	House(){
 		this.onOff = false;
 	}
+	
+	House(String brand, int price){
+		this.brand = brand;
+		this.price = price;
+	}
+	
 	void power() {
 		this.onOff=!this.onOff;
 		System.out.println("전원 : " +this.onOff);
 	}
-	//tv와 에어컨에 만들었던 것을 아예 House로 빼버림.
-	//그럼 tv랑 에어컨과 컴퓨터도 상속을 받아서 여기서 만든 power 메서드를 사용 가능
+	
 }
--- java0704_Start/src/homeProduct/Tv.java
+++ java0704_Start/src/homeProduct/Tv.java
@@ -3,6 +3,19 @@ package homeProduct;
 public class Tv extends House {
 //tv뒤에 바로 1칸 띄고 쓰는거다
 //extends House : House에 작성했던 코드를 여기로 끌고 오는거임.
-	//Tv()	//생성자 메서드
-		//new House();
+	
+	int channel =10;
+	int vol =5;
+	
+	Tv(){		//생성자 메서드
+		super();	
+	}
+	
+	Tv(String brand, int price){//매개변수가 있는 생성자 메서드
+			
+	}
+	
+	void channelUp() {
+		this.channel++;
+	}
 }
